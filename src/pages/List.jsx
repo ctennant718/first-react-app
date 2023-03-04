11e3adc1c61edd0213e3f084ed310d214893d99d
Charlotte Tennant <charlottetennant@Charlotte-Ts-MacBook-Air-2.local>
2023-03-04 10:20:28 +0000

    added context

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useContext, useEffect} from "react";
 // import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 import List from "@mui/material/List";
@@ -12,23 +12,14 @@ import EditIcon from "@mui/icons-material/Edit";
 // import FolderIcon from "@mui/icons-material/Folder";
 import Typography from "@mui/material/Typography";
 
+import { CarsContext } from "../components/contexts/car.context";
+
 function CarsList() {
-  const cars = [
-    {
-      _id: 1,
-      name: "Polo",
-      bhp: 1234,
-      avatar_url:
-        "https://images.clickdealer.co.uk/blog/1311/post_images/d6c690e771f5bf59fc96fccb014d68be.jpg",
-    },
-    {
-      _id: 2,
-      name: "Mini",
-      bhp: 5678,
-      avatar_url:
-        "https://www.topgear.com/sites/default/files/cars-car/image/2022/11/mini%20countryman.jpeg",
-    },
-  ];
+const {cars, fetchCars} = useContext(CarsContext)
+
+useEffect(() => {
+  fetchCars();
+}, [fetchCars] )
   return (
     <>
       <Typography variant="h3" component="h2">
